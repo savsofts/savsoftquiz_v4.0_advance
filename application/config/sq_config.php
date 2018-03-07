4dd2cff2ae25967df56bc742f4027f34926b7461
savsofts <gurpinder@savsoft.info>
2018-03-08 00:21:43 +0530

    updated config file

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
 <?php 
-		$sq_base_url='http://localhost/savsoftquiz_v4.0_advance/';
-		$sq_hostname='localhost';
-		$sq_dbname='savsoftquiz_v4.0';
-		$sq_dbusername='root';
+		$sq_base_url='';
+		$sq_hostname='';
+		$sq_dbname='';
+		$sq_dbusername='';
 		$sq_dbpassword='';
 		?>
---
+++ application/logs/log-2018-03-08.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-03-08 00:20:06 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
