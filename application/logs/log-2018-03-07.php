104e42ecd4a67e4c6c0cb0bc4e9c8b97d06f9488
savsofts <gurpinder@savsoft.info>
2018-03-08 00:02:30 +0530

    Improved installation process

---
+++ application/logs/log-2018-03-07.php
@@ -0,0 +1,63 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-03-07 21:58:03 --> Severity: Warning --> file_put_contents(./application/config/sq_config.php): failed to open stream: Permission denied /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 44
+ERROR - 2018-03-07 21:58:03 --> Severity: Warning --> mysqli_connect(): (HY000/1049): Unknown database 'savsoftquiz_v4.2' /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 56
+ERROR - 2018-03-07 21:58:39 --> Severity: Warning --> file_put_contents(./application/config/sq_config.php): failed to open stream: Permission denied /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 44
+ERROR - 2018-03-07 21:58:39 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 80
+ERROR - 2018-03-07 21:58:39 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 81
+ERROR - 2018-03-07 21:58:39 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 82
+ERROR - 2018-03-07 21:58:39 --> Severity: Warning --> rename(./application/controllers/Install.php,./application/controllers/renamed_Install.php): Permission denied /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 83
+ERROR - 2018-03-07 21:59:28 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 80
+ERROR - 2018-03-07 21:59:28 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 81
+ERROR - 2018-03-07 21:59:28 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 82
+ERROR - 2018-03-07 21:59:28 --> Severity: Warning --> rename(./application/controllers/Install.php,./application/controllers/renamed_Install.php): Permission denied /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 83
+ERROR - 2018-03-07 21:59:32 --> Severity: Warning --> file_get_contents(application/controllers/Payment_gateway_2.php): failed to open stream: No such file or directory /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 7
+ERROR - 2018-03-07 21:59:32 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 213
+ERROR - 2018-03-07 22:00:04 --> Could not find the language line "hello"
+ERROR - 2018-03-07 22:00:04 --> Could not find the language line "user_id"
+ERROR - 2018-03-07 22:00:04 --> Could not find the language line "notattempted"
+ERROR - 2018-03-07 22:00:16 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/models/Quiz_model.php 729
+ERROR - 2018-03-07 22:00:58 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/profile.php 110
+ERROR - 2018-03-07 22:00:58 --> Severity: Warning --> Invalid argument supplied for foreach() /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/profile.php 199
+ERROR - 2018-03-07 22:01:05 --> Severity: Warning --> file_get_contents(application/controllers/Payment_gateway_2.php): failed to open stream: No such file or directory /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 7
+ERROR - 2018-03-07 22:01:05 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 213
+ERROR - 2018-03-07 22:02:05 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 207
+ERROR - 2018-03-07 22:25:24 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 219
+ERROR - 2018-03-07 22:25:53 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 220
+ERROR - 2018-03-07 22:26:18 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 220
+ERROR - 2018-03-07 22:27:40 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 220
+ERROR - 2018-03-07 22:27:57 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 220
+ERROR - 2018-03-07 22:30:54 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 222
+ERROR - 2018-03-07 22:31:33 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 222
+ERROR - 2018-03-07 22:31:47 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 222
+ERROR - 2018-03-07 22:32:26 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 217
+ERROR - 2018-03-07 22:34:16 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 217
+ERROR - 2018-03-07 22:35:19 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 22:36:05 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:25:38 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:25:55 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:26:16 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:27:28 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:27:44 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:31:29 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:31:41 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:32:33 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:35:55 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:37:11 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:38:02 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:39:20 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:39:41 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:40:03 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:40:32 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:43:42 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:43:55 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:44:40 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:45:32 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:46:04 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:46:37 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:47:15 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:49:43 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:50:09 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:50:40 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:51:24 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-07 23:51:50 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
