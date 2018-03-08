e51877b84d8524013baba65e83702906ae76697a
savsofts <gurpinder@savsoft.info>
2018-03-08 16:08:57 +0530

    Added label for enterprise version features

--- application/logs/log-2018-03-08.php
+++ application/logs/log-2018-03-08.php
@@ -1,3 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2018-03-08 00:20:06 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-08 15:47:09 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 80
+ERROR - 2018-03-08 15:47:09 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 81
+ERROR - 2018-03-08 15:47:09 --> Severity: Warning --> chmod(): Operation not permitted /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 82
+ERROR - 2018-03-08 15:47:09 --> Severity: Warning --> rename(./application/controllers/Install.php,./application/controllers/renamed_Install.php): Permission denied /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/controllers/Install.php 83
+ERROR - 2018-03-08 15:47:12 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
+ERROR - 2018-03-08 16:07:13 --> Severity: Warning --> Division by zero /opt/lampp/htdocs/savsoftquiz_v4.0_advance/application/views/dashboard.php 231
